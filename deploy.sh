719341aa4c37cb5c0d4ee81b79780e4d49611394
Teemu Koivisto <tkoivisto456@gmail.com>
2017-10-19 14:04:01 +0300

    Add travis deployment to s3

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Copy the build into bucket and delete all existing files
+aws s3 sync ./dist s3://deep-shrooms-frontend \
+  --region eu-central-1 \
+  --acl bucket-owner-full-control \
+  --cache-control max-age=2592000 \
+  --delete
+
+# Copy index.html the second time with decreased cache duration
+aws s3 cp ./dist/index.html s3://deep-shrooms-frontend \
+  --region eu-central-1 \
+  --acl bucket-owner-full-control \
+  --cache-control max-age=120
