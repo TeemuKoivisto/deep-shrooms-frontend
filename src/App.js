bc0b9935a2391785056f6cc99052e5b0bf689faf
Teemu Koivisto <tkoivisto456@gmail.com>
2017-10-19 14:59:45 +0300

    Add react-webscam with test component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
+import Webcam from 'react-webcam';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    imageSrc: '',
+  }
+
+  setRef = (webcam) => {
+    this.webcam = webcam
+  }
+
+  capture = () => {
+    this.setState({
+      imageSrc: this.webcam.getScreenshot()
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,6 +29,15 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Picture size: {this.state.imageSrc.length}</p>
+        <Webcam
+          audio={false}
+          height={350}
+          ref={this.setRef}
+          screenshotFormat="image/jpeg"
+          width={350}
+        />
+        <button onClick={this.capture}>Capture photo</button>
       </div>
     );
   }
